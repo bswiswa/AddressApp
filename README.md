8050d1b8e8e33785c37241113bd2e1be1155b84d
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-11-26 16:55:30 -0500

    Initial Commit - Complete Part 1: Scene Builder

---
+++ README.md
@@ -0,0 +1,17 @@
+#JavaFX Tutorial by markojakob
+In this project an address app will be built using JavaFX
+Link to tutorial: http://code.makery.ch/library/javafx-8-tutorial/
+
+##Topics to be covered:
+- Creating and starting a JavaFX project
+- Using Scene Builder to design the user interface
+- Structuring an application with the Model-View-Controller (MVC) pattern
+- Using ObservableLists for automatically updating the user interface
+- Using TableView and reacting to selection changes in the table
+- Create a custom popup dialog to edit persons
+- Validating user input
+- Styling a JavaFX application with CSS
+- Persisting data as XML
+- Saving the last opened file path in user preferences
+- Creating a JavaFX chart for statistics
+- Deploying a JavaFX application as a native package
---
+++ src/address/MainApp.java
@@ -0,0 +1,63 @@
+package address;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApp");
+
+		initRootLayout();
+		showPersonOverview();
+	}
+
+	public void initRootLayout() {
+		try {
+			// load root layout from fxml file
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// show the scene containing the root layout
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showPersonOverview() {
+		try {
+			// load root layout from fxml file
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
+
+			// set personOverview into center of rootLayout
+			rootLayout.setCenter(personOverview);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//returns main stage
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
